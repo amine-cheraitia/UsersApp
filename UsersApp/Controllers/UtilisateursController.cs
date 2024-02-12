41b2d5d368e8504e268883f86b6ba3079d8523e9
amine-cheraitia <amine-cheraitia@hotmail.com>
2024-02-12 15:10:06 +0100

    Ajoutez des fichiers projet.

---
+++ UsersApp/Controllers/UtilisateursController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UsersApp.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UsersApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UtilisateursController : ControllerBase
+    {
+        // GET: api/<UtilisateursController>
+        string filePath = @".\Data\utilisateurs.json";
+        
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Utilisateur>> GetAll()
+        {
+            var utilisateurs = JsonConvert.DeserializeObject<List<Utilisateur>>(System.IO.File.ReadAllText(filePath));
+            return Ok(utilisateurs);
+
+        }
+        /*
+        // GET api/<UtilisateursController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UtilisateursController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UtilisateursController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UtilisateursController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }*/
+    }
+}
---
+++ UsersApp/Models/Utilisateur.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsersApp.Models
+{
+    public class Utilisateur
+    {
+
+            [Key]
+            public string? Id { get; set; }
+            public string? Nom { get; set; }
+            public string? Prenom { get; set; }
+            public string? Login { get; set; }
+            public string? MotDePasse { get; set; }
+
+        
+    }
+}
